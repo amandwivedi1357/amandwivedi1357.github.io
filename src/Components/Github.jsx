11bccf0b5d2e3ee353fb51eeedd4970e079f4fb0
Aman Dwivedi <amandwivedi71124.com>
2023-02-28 23:46:14 +0530

    portfolio

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -7,24 +7,15 @@ const Github = () => {
     <div name='github' className='w-full h-full bg-white'>
         <div className=' p-10 max-w-screen-lg mx-auto flex flex-col justify-center w-full h-screen'>
         <div className="pb-8 mt-20 md:mt-20" >
-            <p className="text-4xl font-bold mt-80">Github</p>
+            <p className="text-4xl font-bold">Github</p>
         </div>
-        <div  style={{width:"full",display:"grid",gridTemplateColumns:"repeat(1,1fr)",gap:"15px",textAlign:"center",padding:"40px"}} >
-             <GitHubCalendar
-            username="amandwivedi1357"
-            blockSize={20}
-            blockMargin={5}
-            fontSize={18}
-          />  
-          <div style={{display:"flex",margin:"auto", fontWeight: "700", fontSize: "27px",textAlign:"center",marginTop:"30px"}}>
-          Username : <span style={{  display:"flex",gap:"10px",marginLeft:"5px"}}>  amandwivedi1357 </span>
-          </div> 
-       <img style={{margin:"auto",padding:"10px"}} align={'center'} src="https://github-readme-stats.vercel.app/api?username=amandwivedi1357&show_icons=true&locale=en&border_radius=2&theme=light" alt="amandwivedi1357"></img>
-       <img style={{margin:"auto",padding:"10px"}} align={'center'} src="https://github-readme-streak-stats.herokuapp.com/?user=amandwivedi1357&show_icons=true&locale=en&border_radius=2&theme=light" alt="amandwivedi1357"></img>
-       
+            <div className=' p-10 shadow-md hover:scale-105 duration-500 shadow-blue-600 rounded-xl'>
+            <GitHubCalendar username='amandwivedi1357' ></GitHubCalendar>
+            </div>
+            <div className=' p-10 mt-10 flex justify-center shadow-md hover:scale-105 duration-500 shadow-blue-600 rounded-xl'>
+                <img src="https://github-readme-streak-stats.herokuapp.com?user=amandwivedi1357&theme=nightowl&hide_border=true&border_radius=0&date_format=M%20j%5B%2C%20Y%5D" alt="streak" className='align-center' />
+            </div>
         </div>
-        </div>
-        
       
     </div>
   )
