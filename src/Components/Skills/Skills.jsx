019afc59d9cc84b04232064ffac1b352ccc26ef0
Aman Dwivedi <amandwivedi71124.com>
2023-02-28 11:01:33 +0530

    added

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -59,6 +59,61 @@ const Skills = () => {
             <h1>mongo</h1>
         </div>
       </div>
+
+      <div className="qualifications-container">
+        <div className="qualification">
+            <div className="content html"></div>
+            <h1>html</h1>
+        </div>
+        <div className="qualification">
+            <div className="content css"></div>
+            <h1>css</h1>
+        </div>
+        <div className="qualification">
+            <div className="content js"></div>
+            <h1>javascript</h1>
+        </div>
+        <div className="qualification">
+            <div className="content bootstrap"></div>
+            <h1>chakra-ui</h1>
+        </div>
+        <div className="qualification">
+            <div className="content react"></div>
+            <h1>react</h1>
+        </div>
+        <div className="qualification">
+            <div className="content c-plus-plus"></div>
+            <h1>github</h1>
+        </div>
+        <div className="qualification">
+            <div className="content c-sharp"></div>
+            <h1>cypress</h1>
+        </div>
+        <div className="qualification">
+            <div className="content kotlin"></div>
+            <h1>npm</h1>
+        </div>
+        <div className="qualification">
+            <div className="content typescript"></div>
+            <h1>typescript</h1>
+        </div>
+        <div className="qualification">
+            <div className="content redux"></div>
+            <h1>redux</h1>
+        </div>
+        <div className="qualification">
+            <div className="content node"></div>
+            <h1>node js</h1>
+        </div>
+        <div className="qualification">
+            <div className="content express"></div>
+            <h1>express</h1>
+        </div>
+        <div className="qualification">
+            <div className="content mongo"></div>
+            <h1>mongo</h1>
+        </div>
+      </div>
      
     </div>
   )
